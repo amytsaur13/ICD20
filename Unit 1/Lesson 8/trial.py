acb6b16271e94243e6de7071eacf7bec825df322
Amy Tsaur <ATsaur@bayviewglen.ca>
2023-10-10 15:32:00 -0400

    testing commit and push

--- Unit 1/Lesson 8/trial.py
+++ Unit 1/Lesson 8/trial.py
@@ -1,3 +1,4 @@
 print(f"{'Opponent':<20}{'Your Points':>15}{'Opponent Points':>25}{'Box %':>15}")
 print("==============================================================================")
 print(f"{'Henry':<23}{'Steve':>6 t}")
+# comment

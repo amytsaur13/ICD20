3cf1d5941428f000f107fde97026412157f564d2
Amy Tsaur <ATsaur@bayviewglen.ca>
2023-10-10 10:45:59 -0400

    test 3 oct 10

---
+++ Unit 1/Lesson 7/decimals.py
@@ -0,0 +1,12 @@
+value = 12.3456
+print(f"{value:.2f}")
+
+number = 42
+print(f"{number:<5}")
+
+long_text = "This is a string"
+print(f"{long_text: .15}")
+
+name = "alice"
+age = 28
+print(f"Name: {name>10} Age: {age}")
